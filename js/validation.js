90b509f0bb7a0504a7f4768c634882712ec35b6c
Nikhil <132730216+Megneton555@users.noreply.github.com>
2023-08-01 20:16:43 +0530

    Shopping-Website

--- js/validation.js
+++ js/validation.js
@@ -1,28 +1,31 @@
-const displayError=document.getElementById('error');
-const displaySucess=document.getElementById('sucess');
-const password1=document.getElementById('pass1')
-const password2=document.getElementById('pass2')
+const mobile=document.getElementById("num");
+const password=document.getElementById("pass");
+const confirmpassword=document.getElementById("cpass");
 
-
-
-password2.addEventListener('input', (e)=>{
-    const pass1=password1.value;
-    const pass2=password2.value;
-
- 
-    if(pass1 !== pass2){
-      displayError.innerText="Password not matched!";
-    } else{
-        displaySucess.innerText="Password matched";
-        displayError.style.display="none";
+mobile.addEventListener('keyup',()=>{
+       let number=mobile.value  
+    if(number.length==10){
+        document.getElementById('number').innerText=""; 
+    }else{
+        document.getElementById('number').innerText="Enter a valid number"; 
     }
+})
 
-    e.preventDefault();
+password.addEventListener('keyup',()=>{
+    let pass=password.value  
+    if((pass.length>4) && (pass.length<15)){
+    document.getElementById('password').innerText="";
+ }else{
+    document.getElementById('password').innerText="Characters should be between 5 to 15"; 
+ }
+})
 
-  });
-   
-
-
-
-
-
+confirmpassword.addEventListener('keyup',()=>{
+    let pass=password.value  
+    let cpass=confirmpassword.value
+ if(cpass == pass){
+    document.getElementById('confirmpass').innerText=""; 
+ }else{
+    document.getElementById('confirmpass').innerText="Password not matched!"; 
+ }
+})
